50acc311e70d2cfbdc3d5a3475520af0bc40fdac
Ted Fitzpatrick <tedfitzpatrick@me.com>
2022-07-11 13:15:08 -0700

    added svg icon

--- portfoliosite/src/App.js
+++ portfoliosite/src/App.js
@@ -1,11 +1,30 @@
+import SvgIconPerson from './assets/svgiconperson';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Ted Fitzpatrick <span className='tf-title'>Senior Front-End Designer / Developer</span></h1>
+        <h1><span>Ted Fitzpatrick </span><span className='tf-title'>Senior Front-End Designer / Developer</span></h1>
+        <a id='tf-contactlink'><SvgIconPerson /></a>
         </header>
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Visuals</a></li>
+            <li><a href="#">UI</a></li>
+            <li><a href="#">Code</a></li>
+            <li><a href="#">About</a></li>
+            </ul>
+        </nav>
+        <main>
+
+
+
+
+
+        </main>
     </div>
   );
 }
