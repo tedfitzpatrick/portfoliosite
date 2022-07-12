99ebc0fad3267d619bc2d42c8777bd5202b66142
Ted Fitzpatrick <tedfitzpatrick@me.com>
2022-07-11 20:30:27 -0700

    started home screen

--- portfoliosite/src/screens/HomeScreen.js
+++ portfoliosite/src/screens/HomeScreen.js
@@ -1,13 +1,25 @@
 import React from "react";
 
+import SvgIconMapPointer from './../assets/svgiconmappointer';
+
 const HomeScreen = () => {
 
     return (
         
-        <div className="tf-mainwrapper tf-mainwrapperhome">    
+        <>
+        <div className="tf-homedelivering"><p><SvgIconMapPointer /> Delivering prime UI</p></div>
+        <div className="tf-mainwrapper">    
+        <div className="tf-hero tf-herohome">
         <h1>Home Screen</h1>
         </div>
-        
+        <div className="tf-cards">
+        <div className="tf-card">Visuals</div>
+        <div className="tf-card">UI</div>
+        <div className="tf-card">Code</div>
+        <div className="tf-card">About</div>
+        </div>
+        </div>
+        </>
         
         
     );
